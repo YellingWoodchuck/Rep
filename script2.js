ed901ef55828dd378f56384d99cf19288ac18429
YellingWoodchuck <123351378+YellingWoodchuck@users.noreply.github.com>
2023-02-13 23:49:37 +0200

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,28 @@
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+const time = ["08.15-09.50", "10.00-11.35", "12.30-14.05", "14.15-15.50", "16.00-17.35", "17.45-19.15"]
+
+const template = "Diena: ";
+const template2 = "Days before execution: ";
+
+const subjectMon = [
+    ["KUĢU DĪZEĻIEK. UN TURBĪNAS", 0, 2, "125", 5, 18],
+    ["MATEMĀTIKA", 1, 2, "306"],
+    ["MATEMĀTIKA", 2, 2, "306"],
+];
+const subjectTue = [
+    ["MATER. TEHNOL.", 1, 2, "B2", 14, 17],
+    ["MATER. TEHNOL.", 2, 2, "B2", 1, 6, 13, 18],
+    ["MATER. TEHNOL.", 3, 2, "B2", 1, 6, 13, 14],
+    ["MATER. TEHNOL.", 4, 2, "B2", 1, 6, 13, 14]
+];
+const subjectWed = [
+    ["TERMODINAMIKA UN SILTUMPĀRV.", 2, 2, "114"],
+    ["ANGĻU VALODA", 3, 1, "215"],
+    ["TERMODINAMIKA UN SILTUMPĀRV.", 3, 0,"114"]
+];
+const subjectThu = [
+    ["ANGĻU VALODA", 2, 2, "220"]
+];
+const subjectFri = [
+];
