788b1cc2e07eb56437b2a175cfe718345ffc8020
YellingWoodchuck <123351378+YellingWoodchuck@users.noreply.github.com>
2023-02-14 12:50:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,42 +3,28 @@ window.onload = function() {
     const d = new Date();
     let month = months[d.getMonth()];
     let date = d.getDate();
-    //document.getElementById("TimeLeft").innerHTML = template + month + " " + date;
-    
 
-
-    // JavaScript program to illustrate 
-    // calculation of no. of days between two date 
-
-    // To set two dates to two variables
     var date1 = new Date();
     var date2 = new Date("05/26/2023");
+    var date3 = new Date("01/16/2023");
 
-    // To calculate the time difference of two dates
     var Difference_In_Time = date2.getTime() - date1.getTime();
-
-    // To calculate the no. of days between two dates
     var Difference_In_Days = Math.floor(Difference_In_Time / (1000 * 3600 * 24));
-
-    //To display the final no. of days (result)
     document.getElementById("counter").innerHTML = template2 + Difference_In_Days;
 
-
-    var date3 = new Date("01/16/2023");
-    var text;
     currentweek = Math.floor(Math.ceil((date1 - date3) / (1000 * 3600 * 24)) / 7) + 1;
     weeknumber = Math.floor(Math.ceil((date1 - date3) / (1000 * 3600 * 24)) / 7) % 2 +1;
     
     document.getElementById("CurrentDay").innerHTML = template + month + " " + date + ", " + currentweek + ". nedēļa";
 
     var dom = document.getElementById("tbody");
-    const mon = [];
-    arrayToObj(subjectMon, mon);
-    arrayToObj(subjectTue, mon);
-    arrayToObj(subjectWed, mon);
-    arrayToObj(subjectThu, mon);
-    arrayToObj(subjectFri, mon);
-    tableConstr(mon, dom);
+    const src = [];
+    arrayToObj(subjectMon, src);
+    arrayToObj(subjectTue, src);
+    arrayToObj(subjectWed, src);
+    arrayToObj(subjectThu, src);
+    arrayToObj(subjectFri, src);
+    tableConstr(src, dom);
     if (weeknumber == 0) {
         document.getElementById("even").style.color = "red";
     }
@@ -64,10 +50,23 @@ function createNode(parent, element, colspan = 1, day = 10) {
     let d = new Date();
     if (day == d.getDay()-1) {
         data.setAttribute("style", "color: red");
+        data.setAttribute("class", "current");
     }
     let node = document.createTextNode(element);
     data.appendChild(node);
     parent.appendChild(data);
+}
+
+function weekCheck(element, weekstart = 1, weekend = 18, currentweek) {
+    let check = false
+    if (!(currentweek >= weekstart && currentweek <= weekend)) {
+        element.setAttribute("style", "text-decoration-line: line-through");
+    }
+    if (currentweek >= weekstart && currentweek <= weekend) {
+        element.setAttribute("style", "text-decoration-line: none");
+        check = true;
+    }
+    return check;
 }
 
 function arrayToObj(resource, output) {
@@ -82,12 +81,14 @@ function arrayToObj(resource, output) {
 
 function tableConstr(input, dom) {
     let i = 0;
+    let d = new Date();
+    let dd = d.getDay();
+    dd--;
     input.forEach(element => {
         var row = document.createElement("tr");
         createNode(row, days[i], 8, i);
         dom.appendChild(row);
-        i++;
-        element.forEach(element => {
+        element.forEach(element => {         
             if (element.week == 2 || element.week == 1) {
                 row = document.createElement("tr");
                 createNode(row, time[element.time]);
@@ -101,18 +102,26 @@ function tableConstr(input, dom) {
             createNode(row, element.aud);
             if (element.weekstart != 1 || element.weekend != 18) {
                 if (element.weekstart2 != 1 || element.weekend2 != 18) {
-                    createNode(row, element.weekstart + "-" + element.weekend + " / " + element.weekstart2 + "-" + element.weekend2);
+                    createNode(row, element.weekstart + "-" + element.weekend + " / " + element.weekstart2 + "-" + element.weekend2);               
+                    if (!weekCheck(row, element.weekstart, element.weekend, currentweek)) {
+                        weekCheck(row, element.weekstart2, element.weekend2, currentweek);
+                    }
                 }
                 else {
                     createNode(row, element.weekstart + "-" + element.weekend);
+                    weekCheck(row, element.weekstart, element.weekend, currentweek);
                 }
+            }         
+            if (i == (dd)) {
+                console.log(i + " " + dd);
+                row.setAttribute("class", "current");
             }
             if (element.week == 2 || element.week == 0) {
                 dom.appendChild(row);
-            }
-            
+            }           
         }
         )
+        i++;
     }
     )
 }
