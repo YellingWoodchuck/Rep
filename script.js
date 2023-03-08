7b8014fb64a6730a36b6a031a8ba4f7f32549c78
YellingWoodchuck <123351378+YellingWoodchuck@users.noreply.github.com>
2023-03-08 19:00:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -128,7 +128,7 @@ function showFile(i) {
     var dom = document.getElementById("pdf");
     dom.innerHTML = "";
     var frame = document.createElement("iframe");
-    frame.setAttribute("src", "td-formula1.pdf");
+    frame.setAttribute("src", pdfList[i]);
     frame.setAttribute("width", "100%");
     frame.setAttribute("height", "100%");
     dom.appendChild(frame);
